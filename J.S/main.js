9284a3fe4436e7a5a98a529392c60139a7609382
TikvaYosef <tikvayosef13@gmail.com>
2021-10-20 16:46:18 +0300

    commit

--- J.S/main.js
+++ J.S/main.js
@@ -38,15 +38,31 @@
 // myPromise4.catch((rej)=>{console.log(rej)})
 
 
-// let myPromise4 = new Promise((resolve,reject)=>{
-//     if(true){
-//         resolve("yas")
-//     }else{
-//         reject("no")
-//     }
-// })
-// myPromise4.then((res)=>{console.log(res)})
-// myPromise4.catch((rej)=>{console.log(rej)})
+// function promiseFumction(params) {
 
 
+//     return new Promise((resolve, reject) => {
+//         if (params) {
+//             resolve("yes")
+//         } else {
+//             reject("no")
+//         }
+//     })
+// }
+// // promiseFumction(true)
+// //     .then((res) => { console.log(res) })
+// //     .catch((rej) => { console.log(rej) })
+// promiseFumction(false)
+//     .then((res) => { console.log(res) })
+//     .catch((rej) => { console.log(rej) })
 
+
+// function promiseFumction(num) {
+//     return new Promise((resolve, reject) => {
+//         (num > 10) ? resolve("big") : reject("small");
+//     });
+
+// }
+// promiseFumction(4)
+//     .then((res) => { console.log(res) })
+//     .catch((reg) => { console.log(reg) })
