5ee01014c9895929d0ada86b8bd49d2a84cda524
TikvaYosef <tikvayosef13@gmail.com>
2021-10-20 16:17:49 +0300

    commit

---
+++ J.S/main.js
@@ -0,0 +1,52 @@
+
+
+
+// function print(printSomeThing) {
+
+//     printSomeThing()
+// }
+// print(() => { console.log("hello") })
+
+// let myPromise = new Promise((resolve, reject) => {
+
+// });
+
+// let myPromise = new Promise((resolve, reject) => {
+
+//     if (true) {
+//         resolve("hello");
+//     }
+// });
+// myPromise.then((res)=>{console.log(res)})
+
+// let myPromise3 = new Promise((resolve, reject) => {
+//     if (true) {
+//         reject("goodbye")
+//     }
+// })
+// myPromise3.catch((rej) => {console.log(rej)})
+
+
+// let myPromise4 = new Promise((resolve,reject)=>{
+//     if(true){
+//         resolve("yas")
+//     }else{
+//         reject("no")
+//     }
+// })
+// myPromise4.then((res)=>{console.log(res)})
+// myPromise4.catch((rej)=>{console.log(rej)})
+
+
+// let myPromise4 = new Promise((resolve,reject)=>{
+//     if(true){
+//         resolve("yas")
+//     }else{
+//         reject("no")
+//     }
+// })
+// myPromise4.then((res)=>{console.log(res)})
+// myPromise4.catch((rej)=>{console.log(rej)})
+
+
+
